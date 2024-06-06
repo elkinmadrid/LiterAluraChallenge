a60d84730d748df7dbd0da0cea9b3094df39cef2
elkinmadrid <elkinamp@gmail.com>
2024-06-05 19:22:50 -0500

    First commit

---
+++ src/test/java/co/elkinmadrid/literalurachallenge/LiterAluraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.elkinmadrid.literalurachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
